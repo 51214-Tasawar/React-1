9550fef1d1d47063176468db2192b6fb0cef0727
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-05 15:55:09 +0500

    Code

---
+++ my_app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ const App=()=>{
+  return (
+    <div style={{color:"red", flex:"center" , width:50}}>
+      <h5>Testing Demo By me </h5>
+    </div>
+  )
+}
+export default App ;
